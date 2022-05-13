d551a4345c01024bfa4f67fb2e5979832a8ccd2b
OrEliyahu43 <eurekas300@gmail.com>
2022-05-13 22:59:21 +0300

    last

--- script/script.js
+++ script/script.js
@@ -2,34 +2,28 @@
 const mainURL = 'https://capsules-asb6.herokuapp.com/api/teacher/toam';
 const userIdData = 'https://capsules-asb6.herokuapp.com/api/user'
 const table = document.querySelector('table');
+let currentEdit = null;
 
-
-async function fetchData(url1,url2) {
+async function fetchData(url1, url2) {
     const tableHead = document.querySelector('thead');
     const tableBody = table.querySelector('tbody');
     const response = await fetch(url1)
     const dataRepo = await response.json();
 
-    // console.log(dataRepo);
-    // const id = dataRepo[0].id;
-    // const responseUser = await fetch(`${url2}/${id}`)
-    // const responseUserData = await responseUser.json();
     
-
-    //clear table
     tableBody.innerHTML = ""
-    for(row of dataRepo){
+    for (row of dataRepo) {
         const id = row.id;
         const responseUser = await fetch(`${url2}/${id}`)
         const responseUserData = await responseUser.json();
         const rowElement = document.createElement('tr');
-        
-        
-        for(key in responseUserData){
+
+
+        for (key in responseUserData) {
             const cellElement = document.createElement('td');
             cellElement.textContent = responseUserData[key];
             rowElement.appendChild(cellElement);
-            
+
         }
         const editButtonElement = document.createElement('td')
         const editBtn = document.createElement('button')
@@ -47,56 +41,59 @@ async function fetchData(url1,url2) {
             rowElement.remove();
         })
 
-   
+
         table.appendChild(rowElement);
-        editBtn.addEventListener('click', () => {editRow(rowElement,removeBtn,editBtn)});
-        
+        editBtn.addEventListener('click', function () {
+            editRow(rowElement, editBtn, removeBtn)
+            this.removeEventListener('click', arguments.callee);
+        });
+
     }
 
 }
 
-function editRow(row,removebtn,editbtn){
+
+function editRow(row, editbtn, removebtn) {
+    console.log("I am working")
     const fields = row.querySelectorAll('td');
-    for(let i=1; i < fields.length-2 ; i++){
+    for (let i = 1; i < fields.length - 2; i++) {
         const input = document.createElement('input');
         input.value = fields[i].textContent;
         fields[i].innerHTML = '';
-        input.focus();
         fields[i].appendChild(input);
-        
+
     }
     removebtn.innerText = 'cancel'
     editbtn.innerText = 'confirm'
 
-    editbtn.addEventListener('click', () => {confirmChange(row ,editbtn,removebtn)})
+
+    this.removeEventListener('click', arguments.callee);
+
+    editbtn.addEventListener('click', () => { confirmChange(row, editbtn, removebtn) })
+
 }
 
 
-function confirmChange(row,editbtn,removebtn){
+function confirmChange(row, editbtn, removebtn) {
 
     const fields = row.querySelectorAll('td');
-    for(let i=1; i < fields.length-2 ; i++){
+    for (let i = 1; i < fields.length - 2; i++) {
         const input = fields[i].querySelector('input');
-        console.log(input)
-        // console.log(row.querySelectorAll('td'));
-        // console.log(fields);
-        // console.log(fields[i]);
-        // console.log(input);
-        // console.log(textField);
+        const textField = input.value;
+        console.log("check", textField);
         fields[i].textContent = textField;
-        // input.remove()
+        input.remove()
 
-        
+
     }
+
     removebtn.innerText = 'remove';
     editbtn.innerText = 'edit';
 
-    // function cancelEdit(rowElement){
 
-    // }
 
 }
 
 
-fetchData(mainURL,userIdData);
+fetchData(mainURL, userIdData);
 
